74d9ca5a86be4cb7a83ba15586c79092edb5e4f8
nolnjmes <143047633+nolnjmes@users.noreply.github.com>
2023-10-11 17:52:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function validateForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === 'User1' && password === '123') {
+        alert('Login successful!');
+        window.location.href = "home.html";
+        return false;
+    } else {
+        alert('Invalid username or password. Please try again.');
+        return false;
+    }
+}
+function add() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var result = num1 + num2;
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+}
+
+
+function logout() {
+    window.location.href = "index.html";
+}
